b289f43ebe7cd454a7e30abb4940ca40f165cfb1
SSovelich <163519655+Ssovelich@users.noreply.github.com>
2024-09-19 18:21:29 +0300

    task-1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Profile from './components/Profile/Profile'
+import userData from './userData.json'
+
+function App() {
+ 
+
+  return (
+    
+      <div>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      </div>
+      
+  )
+}
+
+export default App
